a6182b15a7e3757ae2c299e9152952b1eb0e9264
ArtemHryn <grynbest@gmail.com>
2022-07-18 14:47:10 +0300

    7th HW has been finished

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+gallery.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems));
+
+function createGalleryMarkup(gallerylist) {
+  return gallerylist
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+let simpleGallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+simpleGallery.on("changed.simplelightbox", function () {
+  document.body.style.backgroundColor = getRandomHexColor()
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
